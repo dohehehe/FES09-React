005587f117591653159df0095ca6809e2c91c437
dohehehe <doheerlob@gmail.com>
2024-03-04 17:08:24 +0900

    add: react build up

--- workspace/ch01-buildup/counter/05/Mylib.js
+++ workspace/ch01-buildup/counter/05/Mylib.js
@@ -1,9 +1,6 @@
 const Mylib = (() => {
-
-/*  지정한 속성과 자식 요소를 가지는 요소 노드를 생성해서 반환 ------------------- */
-
+  // 지정한 속성과 자식 요소를 가지는 요소 노드를 생성해서 반환
   const createElement = (tag, props, ...children) => {
-    
     // 요소 노드 생성
     const elem = document.createElement(tag);
 
@@ -11,7 +8,11 @@ const Mylib = (() => {
     if(props){
       for(const attrName in props){
         const value = props[attrName];
-        elem.setAttribute(attrName, value);
+        if(attrName.startsWith('on')){
+          elem.addEventListener(attrName.toLowerCase().substring(2), value);
+        }else{
+          elem.setAttribute(attrName, value);
+        }
       }
     }
 
@@ -19,6 +20,8 @@ const Mylib = (() => {
     for(let child of children){
       if(typeof child === 'string' || typeof child === 'number'){
         child = document.createTextNode(child);
+      }else if(typeof child === 'function'){
+        child = child();
       }
       elem.appendChild(child);
     }
@@ -26,14 +29,10 @@ const Mylib = (() => {
     return elem;
   };
 
-
-
-/*  루트노드를 관리하는 객체를 생성 --------------------------- */
-    // IIFE
-
+  // 루트노드를 관리하는 객체를 생성
   const createRoot = (rootNode) => {
     return {
-        // 루트노드 하위에 지정한 컴포넌트를 렌더링 한다.
+      // 루트노드 하위에 지정한 컴포넌트를 렌더링 한다.
       render(appFn){
         rootNode.appendChild(appFn());
       }
@@ -42,8 +41,6 @@ const Mylib = (() => {
 
   return { createElement, createRoot };
 })();
-
-
 
 
 export default Mylib;
