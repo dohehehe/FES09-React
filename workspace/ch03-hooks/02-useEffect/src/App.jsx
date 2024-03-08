9f89b80d3e0c9150a48d10719a95ec2ded7bd828
dohehehe <doheerlob@gmail.com>
2024-03-08 23:19:49 +0900

    learn: useEffect

---
+++ workspace/ch03-hooks/02-useEffect/src/App.jsx
@@ -0,0 +1,14 @@
+import Counter from './components/counter'
+import Header from './components/header'
+
+function App() {
+
+  return (
+    <div id="app">
+      <Header />
+      <Counter />
+  </div>
+  )
+}
+
+export default App
