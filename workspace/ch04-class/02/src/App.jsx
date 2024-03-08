04ed78e534f2b3805c30663b39c989eb9ae423b9
dohehehe <doheerlob@gmail.com>
2024-03-08 14:37:08 +0900

    learn: class component and lifecycle

---
+++ workspace/ch04-class/02/src/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import ChildComponent from './FunctionBase';
+
+class App extends Component{
+  render(){
+    return (
+      <ChildComponent />
+
+    );
+  }
+}
+
+export default App;
---
+++ workspace/ch04-class/03/src/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import ChildComponent from './Lifecycle';
+
+class App extends Component{
+  render(){
+    return (
+      <ChildComponent />
+
+    );
+  }
+}
+
+export default App;
