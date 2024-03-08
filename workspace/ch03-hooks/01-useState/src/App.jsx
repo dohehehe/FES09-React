f46f67076873642c83523dddb9cd8f349e743f83
dohehehe <doheerlob@gmail.com>
2024-03-08 10:50:33 +0900

    learn: React Hook - useState

--- workspace/ch03-hooks/01-useState/src/App.jsx
+++ workspace/ch03-hooks/01-useState/src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+
+
 function App(){
-  return (
-    <>
+  const [message, setMessage] = useState('');
+
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
     
-    </>
+  }
+
+
+  return (
+    <div>
+      <input type="text" onChange={handleChange} />
+      <br />
+      <span>입력 메시지:{message} </span>
+    </div>
   );
 }
 
