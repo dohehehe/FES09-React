aa5b1088d27db62c1a084b8a636aabfcbbfd5e69
dohehehe <doheerlob@gmail.com>
2024-03-11 11:30:53 +0900

    add: comment

--- workspace/ch03-hooks/02-useEffect/src/components/counter.jsx
+++ workspace/ch03-hooks/02-useEffect/src/components/counter.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Button from "./Button";
+import Button from "./button";
 import PropTypes from 'prop-types';
 
 Counter.propTypes = {
@@ -10,41 +10,59 @@ function Counter({ children=0 }) {
   const [count, setCount] = useState(Number(children));
   const [num, setNum] = useState(1);
 
-  // 무한 렌더링 발생
+
+
+  /* 무한 렌더링 발생 ------------------------------- */
+        // 랜더링이 끝난 후에 setTimout이 완료되기때문에
+        // 내부의 setCount로 상태 변화가 일어나게 되고 
+        // 다시 Counter 컴포넌트가 실행되는 것을 반복한다
+
   // setTimeout(() => {
   //   setCount(count + 1);
   // }, 1000);
 
-  // 마운트 된 후에 한번만 호출됨
+
+
+
+  /*  useEffect: 마운트 된 후에 한번만 호출됨 ---------------------------- */
+  
   // useEffect(() => {
   //   setTimeout(() => {
   //     setCount(count + 1);
   //   }, 1000);
   // }, []);
 
-  // useEffect(() => {
-  //   console.log('dependencies => 생략. 컴포넌트가 마운트/업데이트 될 때 매번 호출.');
-  // });
 
-  // useEffect(() => {
-  //   console.log('dependencies => []. 컴포넌트가 마운트 될때 한번만 호출.');
-  // }, []);
+    /* dependencies 에 따른 동작 */
 
-  // useEffect(() => {
-  //   console.log('dependencies => [num]. 컴포넌트가 마운트 될때와 업데이트 될때(num 값이 변경되었을 경우에만) 호출.');
-  //   // ajax로 서버에 검색어 전송
-  // }, [num]);
+        // useEffect(() => {
+        //   console.log('dependencies => 생략. 컴포넌트가 마운트/업데이트 될 때 매번 호출.');
+        // });
+
+        // useEffect(() => {
+        //   console.log('dependencies => []. 컴포넌트가 마운트 될때 한번만 호출.');
+        // }, []);
+
+        // useEffect(() => {
+        //   console.log('dependencies => [num]. 컴포넌트가 마운트 될때와 업데이트 될때(num 값이 변경되었을 경우에만) 호출.');
+        //   // ajax로 서버에 검색어 전송
+        // }, [num]);
+
+
 
   useEffect(() => {
     console.log('setup 함수 호출.');
     const timer = setInterval(() => {
       console.log(new Date());
     }, 1000*num);
+    
     return () => {
       console.log('cleanup 함수 호출.');
       clearInterval(timer);
     };
   }, [num]);
+
+
 
   const handleDown = () => {
     setCount(count - num);
@@ -55,6 +73,9 @@ function Counter({ children=0 }) {
   const handleReset = () => {
     setCount(Number(children));
   };
+
+
+
   return(
     <div id="counter">
       <Button color="red" onClick={ handleDown }>-</Button>
