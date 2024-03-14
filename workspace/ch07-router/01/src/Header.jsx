2505d860a7dfc046aedbd5f247c97499f8fef76b
dohehehe <doheerlob@gmail.com>
2024-03-14 11:34:44 +0900

    learn: Router

---
+++ workspace/ch07-router/01/src/Header.jsx
@@ -0,0 +1,16 @@
+import Mylink from './Mylink';
+
+function Header(){
+  return (
+    <header>
+      <h1>리액트 라우터 - 클라이언트 라우팅 직접 구현</h1>
+      <Mylink to="/">home</Mylink>
+      <br/>
+      <Mylink to="/page1">page1</Mylink>
+      <br/>
+      <Mylink to="/page2">page2</Mylink>
+    </header>
+  );
+}
+
+export default Header;
