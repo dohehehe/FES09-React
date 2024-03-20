06f2b2f598ea3c0ef3c1d5f8d284b9e023f678eb
dohehehe <doheerlob@gmail.com>
2024-03-20 13:25:45 +0900

    learn: recoil - state management library

---
+++ workspace/ch08-globalstate/recoil/src/components/Right3.jsx
@@ -0,0 +1,28 @@
+import { countState } from '@recoil/atoms.mjs';
+import { useEffect } from 'react';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+
+function Right3() {
+  useEffect(()=>{
+    console.log('#### Right3 렌더링.');
+  });
+
+  // getter/setter 모두 사용 (구독)
+  // const [count, setCount] = useRecoilState(countState);
+
+  const setCount = useSetRecoilState(countState);
+
+  const countUp = function(step){
+    // setCount(count + step);
+    setCount(count => count + step);
+  };
+
+  return (
+    <div>
+      <h1>Right3</h1>
+      <button onClick={ () => countUp(1) }>+</button>
+    </div>
+  );
+}
+
+export default Right3;
