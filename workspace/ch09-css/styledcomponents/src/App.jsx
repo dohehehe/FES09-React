f3ecc7107628c7b0a7546216cf554328df83a5f3
dohehehe <doheerlob@gmail.com>
2024-03-26 09:03:04 +0900

    learn: css - styled components

--- workspace/ch09-css/styledcomponents/src/App.jsx
+++ workspace/ch09-css/styledcomponents/src/App.jsx
@@ -1,15 +1,14 @@
-import { Button, Submit } from './components/StyledButton'
+import { Button, Submit } from "./components/StyledButton";
 
-function App() {
-
-
+function App(){
   return (
     <>
       <h1>Styled Components</h1>
       <Button size="10px" color="black" backgroundColor="gray">일반 버튼</Button>
+      <Button size="20px" color="black" backgroundColor="gray">일반 버튼</Button>
       <Submit size="24px">submit 버튼</Submit>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- workspace/ch09-css/styledcomponents/src/components/StyledButton.jsx
+++ workspace/ch09-css/styledcomponents/src/components/StyledButton.jsx
@@ -1,42 +1,37 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-Button.propTypes = {
-  children: PropTypes.string.isRequired,
-}
-Submit.propTypes = {
-  children: PropTypes.string.isRequired,
-}
-
-
-
 const ButtonStyle = styled.button`
-  background-color: ${props=> props.backgroundColor || '#4CAF50'}; /* Green background */
-  border: none; /* Remove borders */
-  color:  ${props=> props.color || 'white'}; /* White text */
-  padding: 6px 18px; /* Padding */
-  text-align: center; /* Center text */
-  text-decoration: none; /* Remove underline */
-  display: inline-block; /* Display as inline-block */
-  font-size:${props => props.size || '16px'}; /* Font size */
-  margin: 4px 2px; /* Margin */
-  cursor: pointer; /* Cursor pointer */
-  border-radius: 6px; /* Border radius */
-
+  background-color: ${ props => props.backgroundColor || '#4CAF50' }; 
+  border: none;
+  color: ${ props => props.color || 'white' }; 
+  padding: 6px 18px; 
+  text-align: center; 
+  text-decoration: none;
+  display: inline-block; 
+  font-size: ${ props => props.size || '16px' };
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 6px;
 `;
 
 const ButtonStyleBlue = styled(ButtonStyle)`
   background-color: blue;
-`
+`;
 
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+};
+Submit.propTypes = {
+  children: PropTypes.string.isRequired,
+};
 
-
-function Button ({children, ...rest }){
-  return <ButtonStyle type="button" {...rest}>{children}</ButtonStyle>
+function Button({ children, ...rest }){
+  return <ButtonStyle type="button" { ...rest }>{ children }</ButtonStyle>
 }
 
-function Submit({children, ...rest}){
-  return <ButtonStyle type="submit" {...rest}>{children}</ButtonStyle>
+function Submit({ children, ...rest }){
+  return <ButtonStyleBlue type="submit" { ...rest }>{ children }</ButtonStyleBlue>
 }
 
-export {Button, Submit};
+export { Button, Submit };
