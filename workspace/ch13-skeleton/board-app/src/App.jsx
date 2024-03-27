c5535c7277c45786367c1d0822c955c94e81bb5c
dohehehe <doheerlob@gmail.com>
2024-03-27 09:31:30 +0900

    learn: tailwind, zustand, darkmode

--- workspace/ch13-skeleton/board-app/src/App.jsx
+++ workspace/ch13-skeleton/board-app/src/App.jsx
@@ -1,12 +1,29 @@
 import { RouterProvider } from "react-router-dom";
 import router from "@/routes";
 import { RecoilRoot } from "recoil";
+import useThemeStore from "@zustand/themeStore.mjs";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App(){
+
+  const { isDarkMode } = useThemeStore();
+
+  if(isDarkMode){
+    document.documentElement.classList.add('dark');
+  }else{
+    document.documentElement.classList.remove('dark');
+  }
+
   return (
-    <RecoilRoot>
-      <RouterProvider router={ router } />
-    </RecoilRoot>
+    <QueryClientProvider client={ queryClient }>    
+      <RecoilRoot>
+        <RouterProvider router={ router } />
+      </RecoilRoot>
+      <ReactQueryDevtools initialIsOpen={ false } />
+    </QueryClientProvider>
   );
 }
 
--- workspace/ch13-skeleton/board-app/src/pages/user/Signup.jsx
+++ workspace/ch13-skeleton/board-app/src/pages/user/Signup.jsx
@@ -1,34 +1,43 @@
 import { useForm } from 'react-hook-form';
 import useCustomAxios from '@hooks/useCustomAxios.mjs';
-import { memberState } from '@recoil/user/atoms.mjs';
-import { useSetRecoilState } from 'recoil';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Submit from '@components/Submit';
 
-function Login() {
-  // recoil setter 반환
-  const setUser = useSetRecoilState(memberState);
+function Signup() {
   const axios = useCustomAxios();
   const navigate = useNavigate();
-  const { register, handleSubmit, formState: { errors }, setError } = useForm({
-    values: {
-      email: 'aa@bb.cc',
-      password: '********'
-    }
-  });
+  const { register, handleSubmit, formState: { errors }, setError } = useForm();
 
   const onSubmit = async (formData) => {
     try {
-      const res = await axios.post('/users/login', formData);
-      // 사용자 정보를 recoil에 저장
-      setUser({
-        _id: res.data.item._id,
-        name: res.data.item.name,
-        profile: res.data.item.profileImage,
-        token: res.data.item.token,
-      });
-      alert(res.data.item.name + '님 로그인 되었습니다.');
-      navigate('/'); // 메인페이지로 이동
+      formData.type = 'user';
+
+      console.log(formData);
+
+      // 이미지 먼저 업로드
+      if(formData.profileImage.length > 0){ // 프로필 이미지를 추가한 경우
+        const imageFormData = new FormData();
+        imageFormData.append('attach', formData.profileImage[0]);
+  
+        const fileRes = await axios('/files', {
+          method: 'post',
+          headers: {
+            // 파일 업로드시 필요한 설정
+            'Content-Type': 'multipart/form-data'
+          },
+          data: imageFormData
+        });
+  
+        // 서버로부터 응답받은 이미지 이름을 회원 정보에 포함
+        formData.profileImage = fileRes.data.file.name;
+      }else{
+        // profileImage 속성을 제거
+        delete formData.profileImage;
+      }
+
+      const res = await axios.post('/users', formData);
+      alert(res.data.item.name + '님 회원가입이 완료 되었습니다.\n로그인 후에 이용하세요.');
+      navigate('/users/login');
     } catch (err) { // AxiosError(네트워크 에러-response가 없음, 서버의 4xx, 5xx 응답 상태 코드를 받았을 때-response 있음)
       if(err.response?.data.errors){  // API 서버가 응답한 에러
         err.response?.data.errors.forEach(error => setError(error.path, { message: error.msg }) );
@@ -42,10 +51,27 @@ function Login() {
     <div className="min-w-80 flex-grow flex items-center justify-center">
       <div className="p-8 shadow-md rounded-lg w-full max-w-md dark:bg-gray-600">
         <div className="text-center py-4">
-          <h2 className="text-2xl font-bold text-gray-700 dark:text-gray-200">로그인</h2>
+          <h2 className="text-2xl font-bold text-gray-700 dark:text-gray-200">회원가입</h2>
         </div>
 
         <form onSubmit={ handleSubmit(onSubmit) }>
+          <div className="mb-4">
+            <label className="block text-gray-700 dark:text-gray-200 font-bold mb-2" htmlFor="name">이름</label>
+            <input 
+              type="text" 
+              id="name" 
+              placeholder="이름을 입력하세요"       
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500 dark:bg-gray-700"      
+              { ...register('name', {
+                required: '이름을 입력하세요.',
+                minLength: {
+                  value: 2,
+                  message: '이름을 2글자 이상 입력하세요.'
+                }
+              }) }
+            />
+            { errors.name && <p className="ml-2 mt-1 text-sm text-red-500">{ errors.name.message }</p> }
+          </div>
           <div className="mb-4">
             <label className="block text-gray-700 dark:text-gray-200 font-bold mb-2" htmlFor="email">이메일</label>
             <input 
@@ -75,11 +101,22 @@ function Login() {
               }) } 
             />
             { errors.password && <p className="ml-2 mt-1 text-sm text-red-500">{ errors.password.message }</p> }
-            <Link className="block mt-6 ml-auto text-gray-500 dark:text-gray-300 hover:underline" to="#">비밀번호를 잊으셨나요?</Link>
           </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 dark:text-gray-200 font-bold mb-2" htmlFor="profileImage">프로필 이미지</label>
+            <input 
+              type="file"
+              accept="image/*" 
+              id="profileImage" 
+              placeholder="이미지를 선택하세요"       
+              className="w-full px-3 py-2 border rounded-lg dark:bg-gray-700"      
+              { ...register('profileImage') }
+            />
+          </div>
+
           <div className="mt-14 flex justify-center items-center">
-            <Submit>로그인</Submit>
-            <Link className="ml-8 text-blue-500 hover:underline" to="/users/signup">회원가입</Link>
+            <Submit>회원가입</Submit>
           </div>
         </form>
       </div>
@@ -87,4 +124,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Signup;
